fed1c89efe1e7207099e1408430ec58b9b649f6c
hafsatcodes <65682252+hafsatcodes@users.noreply.github.com>
2022-06-29 17:20:30 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+
+
+
+const App = () => { 
+
+  const object = [ 'hafsat', 'latifa', 'ummi' ]
+  console.log(object)
+
+  let users = []
+
+let result = []
+  
+const getRandomName = async() => {
+  const response = await fetch('https://randomuser.me/api')
+const data = await response.json()
+
+for(let i=0;i<data.results.length;i++) {
+  users.push(data.result[i])
+
+  console.log(data.results[i])
+}
+
+ 
+
+  // console.log(data.results)
+ return data.results
+}
+
+getRandomName()
+
+// console.log(getRandomName())
+
+console.log(users)
+
+  return (
+    <>
+   {/* <h1>{result}</h1> */}
+   {/* <h1>{data.results} </h1> */}
+   {users.map((user) => (
+    <h3 key={user.gender}>{user.gender}</h3>
+  ))}
+   </>
+  )
+}
+
+export default App
